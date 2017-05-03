89fbd3f3a8efa91bbb417af8c2d5c6f238b5136f
Aja Brofferio <aja.brofferio@gmail.com>
2017-05-03 16:49:13 -0700

    adding a new div for user modified text

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     $(".mad").hide();
+    $(".official").hide();
 });
 const color_1 = document.getElementById('color1');
 const magical = document.getElementById('magic');
@@ -9,6 +10,15 @@ $('#rewrite').click(()=>{
     $('#originalOdyssey').hide();
 });
 
+$('#officialButton').click(()=>{
+    $('#offOdyssey').show();
+    $('#madOdyssey').hide();
+});
+
+$('#back').click(()=>{
+    $('#originalOdyssey').show();
+    $('#offOdyssey').hide();
+});
 color_1.addEventListener('mouseenter', ()=>{
     magical.style.color= color_1.value;
 });
