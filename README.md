90ff47c49bc2bd032741e008653511288698e850
Aja Brofferio <aja.brofferio@gmail.com>
2017-05-14 17:39:48 -0700

    updating the passage with user inputs

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # MadLibs
+This is an online madlibs game that displays a passage from a classic novel, allows you to enter inputs to change the story and then update the passage with the user inputs.
+
+This project is currently incomplete.
+
+Things to do:
+-Look into ReactJS and restructure project
+-Add more passages
+-Enhance the styling
+
--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-    $(".mad").hide();
-    $(".official").hide();
+    $("#madOdyssey").hide();
+    $("#offOdyssey").hide();
 });
-const color_1 = document.getElementById('color1');
-const magical = document.getElementById('magic');
+//const color_1 = document.getElementById('color1');
+//const magical = document.getElementById('magic');
 
 $('#rewrite').click(()=>{
     $('#madOdyssey').show();
@@ -13,12 +13,20 @@ $('#rewrite').click(()=>{
 $('#officialButton').click(()=>{
     $('#offOdyssey').show();
     $('#madOdyssey').hide();
+    $('#verb1official').html($('#verb1').val());
+    $('#noun1official').html($('#noun1').val());
+    $('#adjective1official').html($('#adjective1').val());
+    $('#adjective2official').html($('#adjective2').val());
+    $('#verb2official').html($('#verb2').val());
+    $('#adjective3official').html($('#adjective3').val());
+    $('#verb3official').html($('#verb3').val());
+    $('#color1official').html($('#color1').val());
+    $('#adjective4official').html($('#adjective4').val());
 });
 
 $('#back').click(()=>{
     $('#originalOdyssey').show();
     $('#offOdyssey').hide();
 });
-color_1.addEventListener('mouseenter', ()=>{
-    magical.style.color= color_1.value;
-});
+//for every id in input tag push to array of changed words and 
+//then iterate through array concatenate official to their id name and insert the value of the input id to the innerhtml of official id
